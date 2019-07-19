c10fa60e3b78b3e8cda8f4d01a151ea9c944b332
sedies <sedi.september@gmail.com>
2019-07-19 20:06:03 +0900

    study zerocho javascript exercises

--- zerochoJS/6/Rock-Paper-Scissors.js
+++ zerochoJS/6/Rock-Paper-Scissors.js
@@ -1,22 +1,42 @@
 const result = document.querySelector('.result');
+const count = document.querySelector('.count');
 let left = '0'
 const computer = {
   paper:['0','3'],
   sciccors:['-106px','1'],
   rock:['-212px','2']
 }
+const counter = {
+  win : 0,
+  draw : 0,
+  lose : 0
+}
+let {win, draw, lose} = counter;
+count.textContent=`${win}승 ${draw}무 ${lose}패`;
 let replay='';
 const play = () => {
   replay = setInterval(() => {
-  (left === computer.paper[0])
-  ? left = computer.sciccors[0]
-  : (left === computer.sciccors[0])
-  ? left = computer.rock[0]
-  : left = computer.paper[0]
-  document.querySelector('.computer').style.background=`url(./scissor_rock_paper.jpg) ${left} 0 no-repeat`;
-},100);
+    (left === computer.paper[0])
+    ? left = computer.sciccors[0]
+    : (left === computer.sciccors[0])
+      ? left = computer.rock[0]
+      : left = computer.paper[0]
+    document.querySelector('.computer').style.background=`url(./scissor_rock_paper.jpg) ${left} 0 no-repeat`;
+  },100);
 };
 play();
+const results = (string, vari) => {
+  result.innerHTML=string;
+  (vari === 'win')
+  ? win++
+  : (vari === 'draw')
+    ? draw++
+    : (vari === 'lose')
+      ? lose++
+      : false;
+  count.textContent=`${win}승 ${draw}무 ${lose}패`;
+  setTimeout(() => result.innerHTML='' ,1000)  
+}
 document.querySelectorAll('.btn').forEach( btn => {
   btn.addEventListener('click', function() {
     clearInterval(replay);
@@ -24,19 +44,10 @@ document.querySelectorAll('.btn').forEach( btn => {
     const userNum = this.dataset.num;
     const parse = Object.entries(computer).find(v=>v[1][0]===left);
     ([-2, 1].includes(userNum - parse[1][1]))
-    ? (
-      result.innerHTML='이겼습니다!',
-      setTimeout(() => result.innerHTML='' ,1000)
-      )
-    : (userNum - parse[1][1] === 0)
-      ? (
-        result.innerHTML='무승부!',
-        setTimeout(() => result.innerHTML='' ,1000)
-        )
-      : (
-        result.innerHTML='졌어용 ㅠㅠ..!',
-        setTimeout(() => result.innerHTML='' ,1000)
-        )
+    ? results('이겼습니다!', 'win')
+    : (userNum - parse[1][1] === 0) 
+      ? results('무승부!', 'draw')
+      : results('졌어용 ㅠㅠ..!', 'lose') // ([-1, 2].includes(userNum - parse[1][1])) 
   })
 });
 
