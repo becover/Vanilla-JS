61765e1497a28ea75f5fe7e45d7fc90d8127ff02
becover <sedi.september@gmail.com>
2020-05-11 22:18:00 +0900

    feat: text

--- PaintbrushJS/index.js
+++ PaintbrushJS/index.js
@@ -11,12 +11,34 @@ const saveButton = wrap.querySelector(".save");
 const loadButton = wrap.querySelector(".load");
 const undoButton = wrap.querySelector(".undo");
 const redoButton = wrap.querySelector(".redo");
+const fillTextButton = wrap.querySelector(".fillText");
 const CLASS_PICK = "pick";
 
+// const status = {
+//   history: {
+//     undoList: [],
+//     redoList: [],
+//     poppingLastIndex: true,
+//     poppingAfterIndex: true,
+//   },
+//   canvas: {
+//     isPainting: false,
+//     isFilling: false,
+//     isPipetting: false,
+//     isPicking: false,
+//     isWriting: false,
+//   },
+// };
 const undoList = [];
 let redoList = [];
 let poppingLastIndex = true;
 let poppingAfterIndex = true;
+let isPainting = false;
+let isFilling = false;
+let isPipetting = false;
+let isPicking = false;
+let isWriting = false;
+let lastUseBrushShape;
 
 const canvas = wrap.querySelector(".paint__board");
 const ctx = canvas.getContext("2d");
@@ -29,10 +51,10 @@ ctx.fillStyle = "#333";
 ctx.lineWidth = 5;
 
 const colorPicker = wrap.querySelector(".color__board");
-const ctx2 = colorPicker.getContext("2d");
+const pickerCtx = colorPicker.getContext("2d");
 colorPicker.width = 200;
 colorPicker.height = 200;
-let gradient = ctx2.createLinearGradient(0, 0, colorPicker.width, 0);
+let gradient = pickerCtx.createLinearGradient(0, 0, colorPicker.width, 0);
 gradient.addColorStop(0, "rgb(255, 0, 0)");
 gradient.addColorStop(0.15, "rgb(255, 0, 255)");
 gradient.addColorStop(0.33, "rgb(0, 0, 255)");
@@ -40,21 +62,17 @@ gradient.addColorStop(0.5, "rgb(0, 255, 255)");
 gradient.addColorStop(0.68, "rgb(0, 255, 0)");
 gradient.addColorStop(0.82, "rgb(255, 255, 0)");
 gradient.addColorStop(1, "rgb(255, 0, 0)");
-ctx2.fillStyle = gradient;
-ctx2.fillRect(0, 0, colorPicker.width, colorPicker.height);
-gradient = ctx2.createLinearGradient(0, 0, 0, colorPicker.height);
+pickerCtx.fillStyle = gradient;
+pickerCtx.fillRect(0, 0, colorPicker.width, colorPicker.height);
+gradient = pickerCtx.createLinearGradient(0, 0, 0, colorPicker.height);
 gradient.addColorStop(0, "rgba(255,255,255,1)");
 gradient.addColorStop(0.5, "rgba(255,255,255,0)");
 gradient.addColorStop(0.5, "rgba(0,0,0,0)");
 gradient.addColorStop(1, "rgba(0,0,0,1)");
 
-ctx2.fillStyle = gradient;
-ctx2.fillRect(0, 0, colorPicker.width, colorPicker.height);
-
-let isPainting = false;
-let isFilling = false;
-let isPipetting = false;
-let isPicking = false;
+pickerCtx.fillStyle = gradient;
+pickerCtx.fillRect(0, 0, colorPicker.width, colorPicker.height);
+colorPicker.style.cursor = "crosshair";
 
 const colors = [
   "#333",
@@ -93,24 +111,28 @@ function setColor2Canvas({ r, g, b, a }) {
   currentColor.style.backgroundColor = colorString;
   ctx.fillStyle = colorString;
   ctx.strokeStyle = colorString;
+  if (isWriting) {
+    onChangeTextColor();
+  }
 }
 
 function pipetColor(ctx, x, y, e, width = 1, height = 1) {
   stopPipetting();
   if (e.target && e.target.tagName === "CANVAS") {
-    console.log(x, y);
     const imageData = ctx.getImageData(x, y, width, height);
     const rgbaColor = getRgbaByImageData(imageData);
     return setColor2Canvas(rgbaColor);
   }
 }
 
-function handlePipetteButton(e) {
+function handlePipetteButton() {
   isPipetting = true;
+  canvas.style.cursor = "crosshair";
 }
 
 function stopPipetting() {
   isPipetting = false;
+  canvas.style.cursor = "default";
 }
 
 function wheelEventBindBrushSize(e) {
@@ -193,6 +215,7 @@ function handleRightClick(e) {
 function handleCanvasClick(e) {
   isFilling && ctx.fillRect(0, 0, canvas.width, canvas.height);
   isPipetting && pipetColor(ctx, e.offsetX, e.offsetY, e);
+  isWriting && handleFillText(e.offsetX, e.offsetY);
 }
 
 function handleModeButton() {
@@ -206,6 +229,9 @@ function handleModeButton() {
 }
 
 function bindBrushSize() {
+  if (isWriting) {
+    onChangeTextSize();
+  }
   ctx.lineWidth = brushSize.value;
   wrap.querySelector(".paint__toolB label").innerText = brushSize.value;
 }
@@ -220,13 +246,14 @@ function onMouseMove(e) {
   if (!isPainting) {
     ctx.beginPath();
     ctx.moveTo(x, y);
-  } else if (isPainting) {
+  } else {
     ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
 
 function onMouseDown() {
+  if (isWriting) return false;
   isPainting = true;
 }
 
@@ -252,20 +279,28 @@ function pickBindBrush() {
   brushsShape.forEach((shape) =>
     shape.addEventListener("click", function (e) {
       brushsShape.forEach((shape) => shape.classList.remove(CLASS_PICK));
+      lastUseBrushShape = e.currentTarget;
       e.currentTarget.classList.add(CLASS_PICK);
       ctx.lineCap = e.currentTarget.childNodes[1].innerText;
     })
   );
 }
 
+function onChangeCurrentColor(e) {
+  const selectColor = e.target.style.backgroundColor;
+  selectColor && (currentColor.style.backgroundColor = selectColor);
+  ctx.strokeStyle = selectColor;
+  ctx.fillStyle = selectColor;
+  if (isWriting) {
+    onChangeTextColor();
+  }
+}
+
 function addClassPick(e) {
   const arrayColors = Array.from(paletteColors);
   arrayColors.forEach((color) => color.classList.remove(CLASS_PICK));
   e.target.classList.add(CLASS_PICK);
-  e.target.style.backgroundColor &&
-    (currentColor.style.backgroundColor = e.target.style.backgroundColor);
-  ctx.strokeStyle = e.target.style.backgroundColor;
-  ctx.fillStyle = e.target.style.backgroundColor;
+  onChangeCurrentColor(e);
 }
 
 function pickBindColor() {
@@ -298,9 +333,108 @@ function onToggleClassPick(e, clickEl) {
 function handleColorPickerButton(e) {
   const clickEl = e.currentTarget && e.target.tagName === "LI";
   onToggleClassPick(e, clickEl);
-  pipetColor(ctx2, e.offsetX, e.offsetY, e);
+  pipetColor(pickerCtx, e.offsetX, e.offsetY, e);
 }
 
+function createEl(el, name, parent) {
+  const parentEl = parent && document.querySelector(parent);
+  const El = document.createElement(el);
+  const MANUAL = `작성 후 엔터를 치세요`;
+  if (el === "input") {
+    El.type = "text";
+    El.value = MANUAL;
+  } else {
+    El.innerText = MANUAL;
+  }
+  El.classList.add(name);
+  parentEl.appendChild(El);
+  return El;
+}
+
+function getTextareaInfo(textarea) {
+  const text = textarea.innerText;
+  const color = window.getComputedStyle(textarea).getPropertyValue("color");
+  const size = window.getComputedStyle(textarea).getPropertyValue("font-size");
+  let top = window
+    .getComputedStyle(textarea)
+    .getPropertyValue("top")
+    .slice(0, -2);
+  let left = window
+    .getComputedStyle(textarea)
+    .getPropertyValue("left")
+    .slice(0, -2);
+  const height = window
+    .getComputedStyle(textarea)
+    .getPropertyValue("height")
+    .slice(0, -2);
+  top = Number(top) + Number((height + 8) / 3);
+  left = Number(left);
+  return { text, color, size, top, left };
+}
+
+function removeTextarea(el) {
+  return el.remove();
+}
+
+function drawText(text, color, size, top, left) {
+  ctx.textBaseline = "middle";
+  ctx.font = `${size} "Do Hyeon"`;
+  ctx.fillStyle = color;
+  ctx.fillText(text, left + 1, top);
+}
+
+function onChangeTextColor() {
+  const textarea = document.querySelector(".inputBox");
+  if (textarea !== null) {
+    textarea.style.color = currentColor.style.backgroundColor;
+  }
+}
+
+function onChangeTextSize() {
+  const textarea = document.querySelector(".inputBox");
+  if (textarea !== null) {
+    textarea.style.fontSize = `${+brushSize.value}px`;
+    textarea.style.height = `${+brushSize.value}px`;
+  }
+}
+
+function handleFillText(x, y) {
+  if (document.querySelector(".inputBox") === null) {
+    const textarea = createEl("span", "inputBox", ".paint__boardWrap");
+    textarea.style.top = `${y}px`;
+    textarea.style.left = `${x}px`;
+    textarea.style.maxWidth = `${500 - x}px`;
+    textarea.style.height = `${+brushSize.value}px`;
+    textarea.style.color = currentColor.style.backgroundColor;
+    textarea.style.fontSize = `${+brushSize.value}px`;
+    textarea.contentEditable = isWriting;
+    textarea.focus();
+    textarea.addEventListener("keydown", function (e) {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        paintText2canvas(textarea);
+        fillTextButton.classList.remove(CLASS_PICK);
+        lastUseBrushShape.classList.add(CLASS_PICK);
+        stackCanvasHistory();
+      }
+    });
+  }
+}
+
+function paintText2canvas(textarea) {
+  if (isWriting) {
+    isWriting = false;
+    const { text, color, size, top, left } = getTextareaInfo(textarea);
+    removeTextarea(textarea);
+    drawText(text, color, size, top, left);
+  }
+}
+
+function handleFillTextButton(e) {
+  isWriting = true;
+  e.target.classList.add(CLASS_PICK);
+  brushsShape.forEach((shape) => shape.classList.remove(CLASS_PICK));
+}
 function init() {
   createPaletteColors();
   pickBindColor();
@@ -315,6 +449,7 @@ function init() {
   loadButton.addEventListener("change", handleLoadButton);
   undoButton.addEventListener("click", handleUndoHistory);
   redoButton.addEventListener("click", handleRedoHistory);
+  fillTextButton.addEventListener("click", handleFillTextButton);
 }
 
 init();
